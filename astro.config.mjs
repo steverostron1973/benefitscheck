5bd114bfc58261a057e6a342299eb6ce00f031fa
steverostron1973 <steverostron@outlook.com>
2026-07-06 13:26:11 +0100

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   site: 'https://benefitscheck.co.uk',
 });
